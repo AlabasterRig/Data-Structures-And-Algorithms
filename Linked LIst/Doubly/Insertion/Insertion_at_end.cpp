e58410d18015c1d8b3f30857f368b06b0e6df44c
Alabaster <utkrist.j@mgial.com>
2024-01-07 21:07:05 +0530

    Added Insertion In Doubly LL

---
+++ Linked LIst/Doubly/Insertion/Insertion_at_end.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* prev;
+    Node* next;
+};
+
+void push(Node** head, int new_data)
+{
+    Node *new_node = new Node();
+    new_node->data = new_data;
+    new_node->prev = NULL;
+    new_node->next = *head;
+
+    if(*head != NULL)
+    {
+        (*head)->prev = new_node;
+    }
+
+    *head = new_node;
+}
+
+void InsertAtEnd(Node **head, int new_data)
+{
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = NULL;
+
+    if(*head == NULL)
+    {
+        new_node->prev = NULL;
+        *head = new_node;
+        return;
+    }
+
+    Node *last = *head;
+
+    while(last->next != NULL)
+    {
+        last = last->next;
+    }
+
+    last->next = new_node;
+    new_node->prev = last;
+}
+
+void print(Node* head)
+{
+    while(head != NULL)
+    {
+        cout << head->data << "->";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    Node *head = NULL;
+
+    for(int i = 0; i < n; i++)
+    {
+        push(&head, arr[i]);
+    }
+
+    print(head);
+
+    cout << "\n";
+
+    InsertAtEnd(&head, 8);
+    print(head);
+}
